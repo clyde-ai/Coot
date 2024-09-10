59b23644038844874e135bd6d901d7088786b986
Levi Shepherd <lshepherd2011@gmail.com>
2024-09-10 00:27:28 -0500

    add flavor to !roll

--- bot.js
+++ bot.js
@@ -159,7 +159,26 @@ client.on('interactionCreate', async interaction => {
 client.on('messageCreate', async message => {
     if (message.content === '!roll') {
         const roll = Math.floor(Math.random() * 6) + 1;
-        message.reply(`You rolled a ${roll}`);
+        switch (roll) {
+            case 1:
+                message.reply(`:game_die: You rolled a ${roll}... That's terrible. :skull:`);
+                break;
+            case 2:
+                message.reply(`:game_die: You rolled a ${roll}, not great but could be worse! :unamused:`);
+                break;
+            case 3:
+                message.reply(`:game_die: You rolled a ${roll}, average. :neutral_face:`);
+                break;
+            case 4:
+                message.reply(`:game_die: You rolled a ${roll}, not bad! :thinking:`);
+                break;
+            case 5:
+                message.reply(`:game_die: You rolled a ${roll}, now thats what I'm talking about! :saluting_face:`);
+                break;
+            case 6:
+                message.reply(`:game_die: You rolled a ${roll}! What are you, an Ironman?! :rage:`);
+                break;
+        }
     } else if (message.content === '!snake') {
         const gifPath = path.join(__dirname, 'src/images/memes/snake.gif');
         message.reply({ files: [gifPath] });
