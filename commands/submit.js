0319bbc58ec616f3a78428fa1363c082edc30e86
Levi Shepherd <lshepherd2011@gmail.com>
2024-09-10 13:33:56 -0500

    Update submit.js

--- commands/submit.js
+++ commands/submit.js
@@ -189,6 +189,8 @@ module.exports = {
                 const memberName = interaction.member.displayName;
 
                 // Write to the Submissions sheet
+                console.log(`interaction.id: ${interaction.id}`);
+                console.log(`full link: https://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${interaction.id}`);
                 const submissionStatus = `${imagesSubmitted}/${imagesNeeded}`;
                 const submissionData = [teamName, memberName, tileNumber, submissionStatus, proofAttachment.url, new Date().toISOString(), `https://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${interaction.id}`];
                 await googleSheets.writeToSheet('Submissions', submissionData);
