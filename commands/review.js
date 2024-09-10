50c89fa8b0aea5951b3cc3f45b2578e80b6ac019
Levi Shepherd <lshepherd2011@gmail.com>
2024-09-10 12:35:08 -0500

    Update review.js

--- commands/review.js
+++ commands/review.js
@@ -72,7 +72,7 @@ module.exports = {
             }
 
             // Fetch the message with partials enabled
-            const message = await channel.messages.fetch(messageId);
+            const message = await channel.messages.fetch(messageId, { force: true });
             if (!message) {
                 throw new Error('Message not found');
             }
